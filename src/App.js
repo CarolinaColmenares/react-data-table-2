3f7cf2f36e95a0212ef5447dc04226fd5b17fc2e
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-08-22 00:05:16 -0400

    configurando hooks, función y colums para DataTable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import DataTable from 'react-data-table-component';
 import React, { useState, useEffect } from 'react';
+import 'styled-components';
 import './App.css';
+import DataTable, { createTheme } from 'react-data-table-component';
 
 const App = () => {
- 
+  // 1- Configurar los hooks
+  const [users, setUsers] = useState([])
+
+  // 2- Función para mostrar los Datos con Fetch
+
+  const URL = 'https://gorest.co.in/public/v2/users'
+  const showData = async () => {
+    const response = await fetch(URL)
+    const data     = await response.json()
+    console.log(data)
+    setUsers(data)
+  }
+
+  useEffect(() =>{
+    showData()
+  }, [])
+
+  // 3- Configurar las columns para Datatable
+
+  const columns =[
+    {
+      name:'ID',
+      selector: row =>row.id
+    },
+    {
+      name:'NAME',
+      selector: row =>row.name
+    },{
+      name:'EMAIL',
+      selector: row =>row.email
+    },
+  ]
+
+  // 4- Mostrar la Data en DataTable
+
+
   return (
     <div className="App">
+
+      <DataTable 
+        columns={columns}
+        data={users}
+      />
 
     </div>
   );
